a51148698bb3680152f9d08cbc8b9fd205cd6a5b
CODERNSINGH <narendrasingh.coder@gmail.com>
2024-04-15 13:49:50 +0530

    Day 011

--- Video - 05/main.py
+++ Video - 05/main.py
@@ -1,3 +1,19 @@
 # today i learnt about print statement.
+print("Hello world")
+print(348*34)
 
-#Uses ghj
+
+# for new line
+print("hi my name is Narendra Singh \n and i am software engineer")
+
+
+
+# these are commint lines
+'''Hi there i am narendra singh printing this state directly are you sure about that'''
+"""Hi there my name is narendra singh"""
+
+
+print("narendra" , 3,3) #Multilines 
+
+print("he ther i am narendra",3,3,3, sep="~" ) 
+
