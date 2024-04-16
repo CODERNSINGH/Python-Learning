803ebe256a941df86799a0c062997e66106c300e
CODERNSINGH <narendrasingh.coder@gmail.com>
2024-04-16 13:03:15 +0530

    Day 02

---
+++ Video - 09/main.py
@@ -0,0 +1,15 @@
+# Today we are going to learn what is TypeCasting
+# Type casting in computer ko smaj me aaye What we are Doing in this programme this is Known as Type Casting
+
+a = "23"
+b = "53"
+
+print(a+b)
+
+# here Is use of type cast this know as Explicit Type Case Jo Hamene use kita hai
+print(int(a)+ int(b))
+
+
+x = 0.223
+y = 7.665
+print(x+y)
