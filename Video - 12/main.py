70846a70225a1dd9f9a290fe7af4471e21f3f81e
CODERNSINGH <narendrasingh.coder@gmail.com>
2024-04-18 12:29:21 +0530

    Day 04

---
+++ Video - 12/main.py
@@ -0,0 +1,17 @@
+# Today we are going to learn about string slicing
+
+names = "Narendra, Rohan, Ram, Harry"
+# for length
+print(len(names))
+
+print(names[0:8]) #not include 8th index
+
+print(names[0:-3])
+
+print(names[-7:-3])
+
+
+
+#Quick Quiz
+name = "Harry"
+print(name[-4:-2])
