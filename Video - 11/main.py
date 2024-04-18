593871fa309237d9bc1505b215c9337f1bc418f3
CODERNSINGH <narendrasingh.coder@gmail.com>
2024-04-18 12:11:17 +0530

    Day 04

---
+++ Video - 11/main.py
@@ -0,0 +1,23 @@
+# Today we are Going to learn about string
+name = "Narendra"
+print("my name is "+ name)
+
+print('Hello "i am narendra singh"')
+
+a = '''antidfj akldf daflj
+djfdshfk d
+ df kjdf a dsaf dff ad a
+ df ad fs'''
+print(a)
+
+#accesing character 
+print(name[0])
+
+
+for character in a :
+    print(character)
+
+
+
+
+# That's all for Today
