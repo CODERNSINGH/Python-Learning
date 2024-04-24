adf7c6a2cfac2d93b4c5641a4e343aa292d24c30
CODERNSINGH <narendrasingh.coder@gmail.com>
2024-04-24 12:22:37 +0530

    Day 10

--- Video - 05/main.py
+++ Video - 05/main.py
@@ -8,13 +8,13 @@ print("hi my name is Narendra Singh \n and i am software engineer")
 
 
 
-# these are commint lines
-'''Hi there i am narendra singh printing this state directly are you sure about that'''
-"""Hi there my name is narendra singh"""
+# Print Multiple lines
+print('''Hi there i am narendra singh printing this state directly are you sure about that 
+    Hi there my name is narendra singh''')
 
 
 print("narendra" , 3,3) #Multilines 
 
-print("he ther i am narendra",3,3,3, sep="~" ) 
-print("he ther i am narendra",3,3,3, end=" this is end of this lecture" ) 
+print("he ther i am narendra",3,3,3, sep="~" ) #Middele me tillDey aa jyega
+print("he ther i am narendra",3,3,3, end=" this is end of this lecture" ) #end me jo likha vo aa jyega
 
--- Video - 06/main.py
+++ Video - 06/main.py
@@ -1,5 +1,13 @@
 #Today we are going to learn Variable and Data Types
 
+a = 9
+b = 'narendar singh'
+c = True
+d = None
+e = 32423.34
+#Now we are going to Find Type of these using these commond
+
+print(type(a), type(b),type(c),type(d),type(e))
 
 
 #Notes Will update soon
