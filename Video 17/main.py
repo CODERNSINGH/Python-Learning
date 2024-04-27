4a0df6771f1fe4d23b44a2676e6548de9399c0c4
codernsingh <narendrasingh.coder@gmail.com>
2024-04-27 11:57:39 +0530

    Day 11

---
+++ Video 17/main.py
@@ -0,0 +1,17 @@
+# Now we Going to learn About for loop topic Here we Go
+
+name = 'Narenar singh'
+for anything in name:
+    # print(anything)
+    print(anything, end=',')
+
+names = ['Ankit', 'Sayam', 'Babu', 'Raju']
+for i in names:
+    print(i)
+
+# Range
+for n in range(10): #it's Not include 10
+        print(n)
+
+for k in range(1,11,2):
+     print(k)
