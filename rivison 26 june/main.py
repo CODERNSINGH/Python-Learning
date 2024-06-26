8abbb50d0ec22a8303118663773e526f15ef7403
CODERNSINGH <narendrasingh.coder@gmail.com>
2024-06-26 14:15:27 +0530

    Rivision

---
+++ rivison 26 june/main.py
@@ -0,0 +1,36 @@
+'''hi there i am Narendra singh How are you
+i hope You are Fine '''
+
+print("suppose i have to change line so i have to use this \n this will change the line in printing statement")
+
+print("Narendra singh is Good boy",1,4 ,sep="$$" ,end="//")
+
+
+name = "Narendra singh"
+print(type(name))
+
+
+#Type casting
+
+num1 = "32"
+num2 = "34"
+
+num = num1 + num2 #it will join both of them soo
+
+finalnum = int(num1) + int(num2)
+print(num)
+print(finalnum)
+
+
+yourName = input("Enter Your Name: ")
+print("Your Name is :", yourName)
+
+letString = "Hi i am a Boy How are You "
+
+print(letString[0])
+
+#looping in String
+
+for character in letString:
+    print(character)
+
